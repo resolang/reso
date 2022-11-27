df926a15b0338a8fb05cf57b9f52a0216f5060ad
lynn pepin <lynnpepin@tutanota.com>
2022-11-26 21:49:31 -0500

    Loading image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use image::{GenericImageView, ImageBuffer, Rgba, RgbaImage, DynamicImage};
+
+fn load_image_fron_filename(filename: &str) -> DynamicImage {
+    // Load the image from the file (copilot)
+    let img = image::open(filename).expect("File not found, sorry!");
+    let (width, height) = img.dimensions();
+    println!("Loaded {} ({}x{} px).", &filename, width, height);
+    img
+}
+
 fn main() {
-    println!("Hello, world!");
+    let img = load_image_fron_filename("test.png");
+    let (width, height) = img.dimensions();
+    println!("{}x{}", width, height);
 }

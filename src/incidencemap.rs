798e133a21de17aa5ba2a924f7f63d78013abd13
lynn pepin <lynnpepin@tutanota.com>
2023-12-03 22:51:31 -0500

    doc: incidencemap.rs

--- src/incidencemap.rs
+++ src/incidencemap.rs
@@ -1,60 +1,68 @@
-/* incidencemap.rs
+//! incidencemap.rs: Functionality to identify the useful incidences between regions.
+//!  
+//! Used when executing the circuit.
+//!  
+//! Specifically,
+//!   inputs  <- wires  
+//!   logic   <- inputs 
+//!   outputs <- inputs 
+//!   outputs <- logic  
+//!   wires   <- output
+//! 
+//! This makes use of the dense indices provided by Regionmapper.
+//! 
+//! E.g. 
+//!   Reselboard:   xy_to_region:
+//!   ..........    ..........
+//!  :    =lll     :    4888
+//!  :ooo+^        :11135
+//!  :sss+&        :22236
+//!  :    =lll     :    7999
+//!   ..........    ..........
+//! 
+//! 
+//! - wire_regions   = [1,2,8,9]
+//! - input_regions  = [3,]
+//! - logic_regions  = [5,6,]
+//! - output_regions = [4,7,]
+//! - reverse_dense  = [0,0,1,0,0,0,1,1,2,3]
+//! 
+//! We will get:
+//! - input_inc_wires   = [[0,1],]   // because adj(3,1) and adj(3,2)
+//! - logic_inc_inputs  = [[0],[0],] // because adj(5,3) and adj(6,3)
+//! - output_inc_inputs = [[], []]   // because no adj(4,x), no adj(7,x)
+//! - output_inc_logics = [[0], [1]] // because adj(4,5) and adj(7,6)
+//! - wire_inc_outputs  = [[], [], [0], [1]] // because adj(8,4) and adj(9,7)
+//! 
+//! 
+//! These incidence maps are based on the dense index of each list of regions.
+//! 
+//! e.g.                          // region 9 adj region 7
+//! - wire_regions[3]     == 9    // wire_index    3 == region_index 9
+//! - wire_inc_outputs[3] == [1,] // output_region 1 == region_index 7
+//! - output_regions[1]   == 7
+//! 
+//! TODO: From here!
+//! - More tests
+//! - Review / clean this up
+//! - Then, CLI in main
+//! - Then, ARCHITECTURE.md, ALGORITHMS.md, README.md
+//! - Then, ResoCircuit
 
-Functionality to identify the useful incidences between regions.
-
-Specifically,
-  inputs  <- wires  
-  logic   <- inputs 
-  outputs <- inputs 
-  outputs <- logic  
-  wires   <- output
-
-This makes use of the dense indices provided by Regionmapper.
-
-E.g. 
-  Reselboard:   xy_to_region:
-  ..........    ..........
- :    =lll     :    4888
- :ooo+^        :11135
- :sss+&        :22236
- :    =lll     :    7999
-  ..........    ..........
-
-
-- wire_regions   = [1,2,8,9]
-- input_regions  = [3,]
-- logic_regions  = [5,6,]
-- output_regions = [4,7,]
-- reverse_dense  = [0,0,1,0,0,0,1,1,2,3]
-
-We will get:
-- input_inc_wires   = [[0,1],]   // because adj(3,1) and adj(3,2)
-- logic_inc_inputs  = [[0],[0],] // because adj(5,3) and adj(6,3)
-- output_inc_inputs = [[], []]   // because no adj(4,x), no adj(7,x)
-- output_inc_logics = [[0], [1]] // because adj(4,5) and adj(7,6)
-- wire_inc_outputs  = [[], [], [0], [1]] // because adj(8,4) and adj(9,7)
-
-
-These incidence maps are based on the dense index of each list of regions.
-
-e.g.                          // region 9 adj region 7
-- wire_regions[3]     == 9    // wire_index    3 == region_index 9
-- wire_inc_outputs[3] == [1,] // output_region 1 == region_index 7
-- output_regions[1]   == 7
-
-TODO: From here!
-- More tests
-- Review / clean this up
-- Then, CLI in main
-- Then, ARCHITECTURE.md, ALGORITHMS.md, README.md
-- Then, ResoCircuit
-
-*/
 
 use crate::resel::{Resel};
 use crate::reselboard::{ReselBoard};
 use crate::regionmap::{RegionMap};
 
+
+/// IncidenceMap holds mapping of incident dense-region indices,
+/// based on a RegionMap.
+/// 
+/// For example, given `input_inc_wires[2] = [1,2]` means
+/// RegionMap.input_nodes[2] is adjacent to both RegionMap.wire_nodes[1] and
+/// RegionMap.wire_nodes[2].
+/// 
+/// This is used when executing a circuit.
 pub struct IncidenceMap {
   pub input_inc_wires:    Vec<Vec<usize>>,
   pub logic_inc_inputs:   Vec<Vec<usize>>,
@@ -63,6 +71,7 @@ pub struct IncidenceMap {
   pub wire_inc_outputs:   Vec<Vec<usize>>,
 }
 
+/// Returns an IncidenceMap from a RegionMap
 pub fn incidencemap_from_regionmap(
   rm: &RegionMap
 ) -> IncidenceMap {
